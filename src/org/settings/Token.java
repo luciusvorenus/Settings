244d8bb784f3e124892b94669a5e9efaeeacb7c7
Miguel Martins <miguelcardosomartins@gmail.com>
2015-09-15 21:24:07 +0200

    MM: private member variables

--- src/org/settings/Token.java
+++ src/org/settings/Token.java
@@ -6,13 +6,21 @@ package org.settings;
  */
 public class Token {
     
-    final TokenType type;
-    final String    text;
+    private final TokenType type;
+    private final String    text;
 
     public Token(final TokenType type, final String text) {
         this.type = type;
         this.text = text;
     }
+    
+    TokenType getType() {
+        return this.type;
+    }
+    
+    String getText() {
+        return this.text;
+    } 
 
     @Override
     public String toString() {
