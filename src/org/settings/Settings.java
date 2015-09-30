8401db903967c0a5cd843e35d1718c48b9ece52e
Miguel Martins <miguelcardosomartins@gmail.com>
2015-09-30 23:25:40 +0200

    MM: doc

--- src/org/settings/Settings.java
+++ src/org/settings/Settings.java
@@ -5,7 +5,77 @@ import java.util.Arrays;
 import java.util.Collection;
 
 /**
- * Main class to handle gcf-configutation files.
+ * Main class to handle gcf-configutation files.<br>
+ * GCF stands for "Group Configuration File", i.e. the 
+ * data is saved in Groups. This class offers access to those 
+ * groups which in turn offer access to the underlying configuration 
+ * data.<br>
+ * The basic syntax is as follow:<br>
+ * <pre>
+ * --------- GCF START------------
+ *     global_var = "some var"
+ * 
+ *     [Constants]
+ *         int     = 10
+ *         float   = 12.48
+ *         boolean = true
+ *         string  = "some string"
+ *         var     = ${global_var}
+ *         [Sub]
+ *             nr = 1000
+ *         [/Sub]
+ *     [/Constants]
+ * --------- GCF END -------------
+ * </pre>
+ * 
+ * The configuration data is saved as key/value pairs within a group.
+ * The key names can be composed of letters, numbers and underscores, but
+ * must start with a letter. The value can be integers, floats 
+ * (with a dot as the decimal separator), booleans, strings
+ * (enclosed in double quotes) or a reference to a global variable.<br>
+ * The groups are defined using square brackets inbetween which 
+ * the groupname is defined. The rules for the groupname are the 
+ * same as for the keys.<br>
+ * A group has a start and end definition (similar to XML). If 
+ * a group is not closed correctly, either by mispelling of the 
+ * group name, or missing the "group end tag", an exception is thrown,
+ * during parsing.<br>
+ * The content of a group can be key/values or other (sub)groups.<br><br>
+ * 
+ * To get access to a specific group one can use the <code>Settings</code>
+ * instance as follows:
+ * <blockquote>
+ * <pre>
+ *     final Settings set = new Settings(someFile);
+ *     final Group constGroup = set.getGroup("/Constants/");
+ * </pre>
+ * </blockquote>
+ * 
+ * With the group instance one can get access to the configuration data
+ * and/or subgroups.
+ * <blockquote>
+ * <pre>
+ *     final String str = constGroup.readString("string");
+ *     final Group subGroup = constGroup.getGroup("Sub/");
+ * </pre>
+ * </blockquote>
+ * 
+ * Notice how the path to retrieve a sub group is relative.<br><br>
+ * 
+ * Furthermore the Group class allows one to add new keys or 
+ * change the values of existing keys, as well as adding and 
+ * deleting subgroups.<br>
+ * For any of these actions to take effect one has to explicity
+ * save these changes
+ * <blockquote>
+ * <pre>
+ *     // create this key/value inside the Constants group
+ *     constGroup.addKey("some_new_key",9999.99);
+ * 
+ *    // save the changes
+ *    set.save();
+ * </pre>
+ * </blockquote>
  */
 public class Settings {
 
@@ -52,7 +122,7 @@ public class Settings {
     private void checkFileEnding(final String fileName) throws GcfException {
         boolean passed = false;
         for(String ext : FILE_EXTENSIONS) {
-            if (fileName.endsWith(ext)) {
+            if (fileName.endsWith("."+ext)) {
                 passed = true;
                 break;
             }
