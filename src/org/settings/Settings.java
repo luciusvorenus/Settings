31cabc761da7f5cc91830d3f88ef51cd5fd5d837
Miguel Martins <miguelcardosomartins@gmail.com>
2015-10-06 20:00:41 +0200

    MM: doc

--- src/org/settings/Settings.java
+++ src/org/settings/Settings.java
@@ -32,8 +32,10 @@ import java.util.Objects;
  * --------- GCF START------------
  *     global_var = "some var"
  * 
+ *     # Some multiline
+ *     # comment
  *     [Constants]
- *         int     = 10
+ *         int     = 10 # some inline comment
  *         float   = 12.48
  *         boolean = true
  *         string  = "some string"
@@ -45,22 +47,25 @@ import java.util.Objects;
  * --------- GCF END -------------
  * </pre>
  * 
- * The configuration data is saved as key/value pairs within a group.
+ * The configuration data is saved as key/value pairs within groups.<br>
  * The key names can be composed of letters, numbers and underscores, but
- * must start with a letter. The value can be integers, floats 
- * (with a dot as the decimal separator), booleans, strings
- * (enclosed in double quotes) or a reference to a global variable.<br>
- * The groups are defined using square brackets inbetween which 
- * the groupname is defined. The rules for the groupname are the 
- * same as for the keys.<br>
- * A group has a start and end definition (similar to XML). If 
- * a group is not closed correctly, either by mispelling of the 
- * group name, or missing the "group end tag", an exception is thrown,
+ * must start with a letter.<br>
+ * The values can be integers, floats (with a dot as the decimal separator), 
+ * booleans, strings (enclosed in double quotes) or a reference to a 
+ * global variable.<br>
+ * A group is defined using square brackets inbetween which 
+ * the groupname is defined.<br>
+ * It has an opening and closing statement (similar to XML). If 
+ * a group is not closed properly, either by mispelling of the 
+ * group name, or missing the "end group" syntax, an exception is thrown,
  * during parsing.<br>
- * The content of a group can be key/values or other (sub)groups.<br><br>
+ * The rules for the groupname are the same as for the keys, with the
+ * exception that groupnames can start with numbers.<br>
+ * The content of a group can consist of key/values and/or other (sub)groups.<br>
+ * Comments are started with a '#' symbol.<br><br>
  * 
- * To get access to a specific group one can use the <code>Settings</code>
- * instance as follows:
+ * To get access to a specific group one uses the <code>Settings</code>
+ * class as follows:
  * <blockquote>
  * <pre>
  *     final Settings set = new Settings(someFile);
@@ -68,21 +73,32 @@ import java.util.Objects;
  * </pre>
  * </blockquote>
  * 
- * With the group instance one can get access to the configuration data
- * and/or subgroups.
+ * With the Settings instance reference one can get access to the configuration 
+ * data and/or the subgroups.
  * <blockquote>
  * <pre>
+ *     # Get a reference to the "Constants" group
+ *     final Group constGroup = constGroup.getGroup("/Constants/");
+ * 
+ *     # Read a value for the key "string"
  *     final String str = constGroup.readString("string");
+ * 
+ *     # Get a reference to the subgroup "Sub"
  *     final Group subGroup = constGroup.getGroup("Sub/");
  * </pre>
  * </blockquote>
  * 
- * Notice how the path to retrieve a sub group is relative.<br><br>
+ * A group is retrieved using its full path, so as if it was in a 
+ * (Unix) filesystem. A subgroup on the other hand is retrieved using
+ * the relative path, starting at the parent.<br>
+ * To access the value of a key, one passes the key name as a string
+ * to the respective type method, e.g. <code>readInt</code> 
+ * to retrieve an integer.<br><br>
  * 
- * Furthermore the Group class allows one to add new keys or 
- * change the values of existing keys, as well as adding and 
- * deleting subgroups.<br>
- * For any of these actions to take effect one has to explicity
+ * Besides allowing the retrieval of subgroups, the Group class allows 
+ * the adding of new keys or changing the values of existing keys, as well as 
+ * adding and deleting subgroups.<br>
+ * For any of these actions to take effect though, one has to explicitly
  * save these changes
  * <blockquote>
  * <pre>
